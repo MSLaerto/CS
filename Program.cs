6718fe12e9eda4fcb03a7eda701fc0e4a3f3cb52
Maxwell11 <maksikbestbest@gmail.com>
2023-06-11 09:35:03 +0300

    privetstvie

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите имя пользователя: ");    
+string username = Console.ReadLine();
+if (username = "Максим"){
+    Console.WriteLine("Здарова,Босc");
+}else{
+    Console.Write("Добрый день, ");
+    Console.WriteLine("username");
+}
